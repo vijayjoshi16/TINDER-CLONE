843b3395bf87cf87f978314429721a452128f3a0
vijayjoshi16 <joshivijay324@gmail.com>
2020-11-10 12:36:45 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header/Header.js'
+import TinderCards from './Components/TinderCards/TinderCards.js'
+import SwipeButtons from './Components/SwipeButtons/SwipeButtons.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header/>
+      <TinderCards />
+      <SwipeButtons />
     </div>
   );
 }
